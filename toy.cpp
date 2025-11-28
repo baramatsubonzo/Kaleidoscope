e287b393b6160e1e03d63d2596fb80c7510ed49a
Shota Matsubara <baramatsubonzo@gmail.com>
2025-11-29 00:21:28 +0900

    Chapter 4: LLVM Optimization Passes

--- toy.cpp
+++ toy.cpp
@@ -445,6 +445,41 @@ static void HandleTopLevelExpression() {
   }
 }
 
+void InitializeModueleAndManagers(void) {
+  TheContext = std::make_unique<LLVMContext>();
+  TheModule = std::make_unique<Module>("KaleidoscopeJIT", *TheContext);
+  TheModule->setDataLayout(TheJIT->getDataLayout());
+
+  Builder = std::make_unique<IRBuilder<>>(*TheContext);
+
+  TheFPM = std::make_unique<FunctionPassManager>();
+  TheLAM = std::make_unique<LoopAnalysisManager>();
+  TheFAM = std::make_unique<FunctionAnalysisManager>();
+  TheCGAM = std::make_unique<CGSCCAnalysisManager>();
+  TheMAM = std::make_unique<ModuleAnalysisManager>();
+  ThePIC = std::make_unique<PassInstrumentationCallbacks>();
+  TheSI = std::make_unique<StandardInstrumentations>(*TheContext, true);
+
+  TheSI->registerCallbacks(*ThePIC, TheMAM.get());
+
+  TheFPM->addPass(InstCombinerPass());
+  TheFPM->addPass(ReassociatePass());
+  TheFPM->addPass(GVNPass());
+  TheFPM->addPass(SimplifyCFGPass());
+
+  PassBuilder PB;
+  PB.registerModuleAnalyses(*TheMAM);
+  PB.registerFunctionAnalyses(*TheFAM);
+  PB.crossRegisterProxies(*TheLAM, *TheFAM, *TheCGAM, *TheMAM);
+}
+
+if (Value *RetVal = Body->codegen()) {
+    Builder.CreateRet(RetVal);
+    verifyFunction(*TheFunction);
+    TheFPM->run(*TheFunction, *TheFAM);
+    return TheFunction;
+}
+
 static void MainLoop() {
   while (true) {
     fprintf(stderr, "ready> ");
