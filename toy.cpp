36c079957cf64dd755eeb78c25458d51af38da11
Shota Matsubara <baramatsubonzo@gmail.com>
2025-12-26 17:03:02 +0900

    lexer: 可読性のためファイル分割

--- toy.cpp
+++ toy.cpp
@@ -38,76 +38,10 @@
 #include "llvm/IR/LegacyPassManager.h"
 
 #include "token.h"
+#include "lexer.h"
 
 using namespace llvm;
 using namespace llvm::orc;
-
-
-static std::string IdentifierStr;
-static double NumVal;
-
-// Returns the next token from standard input.
-static int gettok() {
-  static int LastChar = ' ';
-  // Skip any whitespace.
-  while (isspace(LastChar))
-    LastChar = getchar();
-
-  if (isalpha(LastChar)) { // The initial character must be a letter(A-Z, a-z). e.g., "1def" is prohibited.a
-    IdentifierStr = LastChar;
-    while (isalnum((LastChar = getchar()))) // Subsequent characters can be letters or numbers.
-      IdentifierStr += LastChar;
-
-    if (IdentifierStr == "def")
-      return tok_def;
-    if (IdentifierStr == "extern")
-      return tok_extern;
-    if (IdentifierStr == "if")
-      return tok_if;
-    if (IdentifierStr == "then")
-      return tok_then;
-    if (IdentifierStr == "else")
-      return tok_else;
-    if (IdentifierStr == "for")
-      return tok_for;
-    if (IdentifierStr == "in")
-      return tok_in;
-    if (IdentifierStr == "binary")
-      return tok_binary;
-    if (IdentifierStr == "unary")
-      return tok_unary;
-    if (IdentifierStr == "var")
-      return tok_var;
-    return tok_identifier;
-  }
-
-  if (isdigit(LastChar) || LastChar == '.') { // e.g., `.14` is equivalent to `0.14`
-    std::string NumStr;
-    do {
-      NumStr += LastChar;
-      LastChar = getchar();
-    } while (isdigit(LastChar) || LastChar == '.');
-
-    NumVal = strtod(NumStr.c_str(), 0); // Convert string to double
-    return tok_number;
-  }
-
-  if (LastChar == '#') {
-    do
-      LastChar = getchar();
-    while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
-
-    if (LastChar != EOF)
-      return gettok();
-  }
-
-  if (LastChar == EOF)
-    return tok_eof;
-
-  int ThisChar = LastChar;
-  LastChar = getchar();
-  return ThisChar;
-}
 
 //===
 // Abstract Syntax Tree (AST)
